ce3fb5a7b82ec3beefb9aa05a1d68d7e66552016
Ken Shoufer <kshoufer@gmail.com>
2014-11-25 21:01:52 -0800

    added single quotes to array names

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@
 		$data = file_get_contents ('data.json');
 		$books_array=json_decode($data,true);
 
-		foreach ($books_array[book] as $book) {
-			if ($book[name] == $find) {
-				return $book[price];
+		foreach ($books_array['book'] as $book) {
+			if ($book['name'] == $find) {
+				return $book['price'];
 				break;
 			}
 		}
